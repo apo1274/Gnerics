8246821083efc2bdfc670cb22a42e5572075ab38
Oğuzhan Turgut <oguz.turgut32@gmail.com>
2021-02-03 13:05:29 +0300

    03/02/2021 Generics

---
+++ Gnerics/MyLıst.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gnerics
+{
+    class MyLıst<T>
+    {
+        T[] items;
+        public MyLıst()
+        {
+            items = new T[0];
+
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ Gnerics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gnerics
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            MyLıst<int> isimler = new MyLıst<int>();
+
+            isimler.Add(10);
+        }
+    }
+}
